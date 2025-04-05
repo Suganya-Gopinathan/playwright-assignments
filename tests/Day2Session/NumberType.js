956a6ee0c4d63b075842d3630c428689197bae29
Suganya-Gopinathan <suganyagopinathan1994@gmail.com>
2025-04-05 19:48:56 +0530

    NumberType Assignment Commit

---
+++ tests/Day2Session/NumberType.js
@@ -0,0 +1,14 @@
+function Number(number) {
+    if (number > 0) {
+        console.log("The Number is Positive");
+    }
+    else if(number < 0){
+        console.log("The Number is Negative");
+    }
+    else
+    {
+        console.log("The Number is Neutral");
+    }
+}
+
+Number(-12);
