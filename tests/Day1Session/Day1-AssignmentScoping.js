6a92768c2c5a2640e7e799d63a04965cb47b4dcc
Suganya-Gopinathan <suganyagopinathan1994@gmail.com>
2025-04-05 20:18:49 +0530

    Day 1 Session Assignment

---
+++ tests/Day1Session/Day1-AssignmentScoping.js
@@ -0,0 +1,17 @@
+const newbrowserVersion = "Chrome";
+
+function getBrowserVersion() {
+
+    if (newbrowserVersion === "Chrome") {
+        let newbrowserVersion = "Safari";
+        console.log("The browser version is " + newbrowserVersion);
+    }
+    else {
+        //var newbrowserVersion = "Chromium";cl
+        console.log("The browser version is " + newbrowserVersion);
+    }
+    console.log("The browser version is " + newbrowserVersion);
+}
+
+getBrowserVersion();
+console.log("The browser version is " + newbrowserVersion);
