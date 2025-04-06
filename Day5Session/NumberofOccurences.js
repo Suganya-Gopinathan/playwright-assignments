c5800579596bbeec9bad1fa0707bb29aec105db0
Suganya-Gopinathan <suganyagopinathan1994@gmail.com>
2025-04-07 00:24:00 +0530

    Committing the Number of occurences code

---
+++ Day5Session/NumberofOccurences.js
@@ -0,0 +1,13 @@
+const nums = [2, 4, 5, 2, 1, 2];
+const k = 2;
+
+let count = 0;
+for (let index = 0; index < nums.length; index++) {
+    if (nums[index] === k) {
+        count++;
+    }
+
+}
+
+console.log(`${count}`);
+
